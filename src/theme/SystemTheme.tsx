959c12a7095d51bfafa91634e9be845a2076e0ae
onichandame <zxinmyth@gmail.com>
2020-06-01 08:10:47 +0000

    sysmode

--- src/theme/SystemTheme.tsx
+++ src/theme/SystemTheme.tsx
@@ -1,22 +1,14 @@
-import React, { FC, useReducer, useEffect } from "react"
-import { MuiThemeProvider, Theme } from "@material-ui/core/styles"
+import React, { FC } from "react"
+import { MuiThemeProvider } from "@material-ui/core/styles"
+import { useSysMode } from "react-sysmode"
 
 import { lightTheme } from "./light"
 import { darkTheme } from "./dark"
 
 export const SystemTheme: FC = props => {
-  const reduce = (): Theme =>
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? darkTheme
-      : lightTheme
-  const [theme, update] = useReducer(reduce, lightTheme)
-  useEffect(() => {
-    update()
-    window.matchMedia("(prefers-color-scheme: dark)").addListener(update)
-  }, [])
+  const { dark } = useSysMode()
   return (
-    <MuiThemeProvider theme={theme}>
+    <MuiThemeProvider theme={dark ? darkTheme : lightTheme}>
       <div {...props} />
     </MuiThemeProvider>
   )
